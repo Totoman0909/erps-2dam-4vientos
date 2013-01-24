7f83d373b298992cb6130d6f1516930ec886f902
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@26be5ec8-3439-2871-35d6-f44324df5b0d>
2013-01-24 08:27:01 +0000

    commit

--- AccesoRed/src/AccesoRedJsoupIbex35.java
+++ AccesoRed/src/AccesoRedJsoupIbex35.java
@@ -14,7 +14,7 @@ import org.jsoup.select.Elements;
  * @author pello_altadill
  *
  */
-public class AccesoRedJsoup {
+public class AccesoRedJsoupIbex35 {
 
 	/**
 	 * @param args
@@ -22,17 +22,25 @@ public class AccesoRedJsoup {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String url = "http://www.as.com";
+		String url = "http://www.expansion.com/mercados/cotizaciones/indices/ibex35_I.IB.html";
 		// importar Document de la librería jsoup
 		try {
-			// establecer conexión
 			
 			// recoger la página en un documento
 			Document documento = Jsoup.connect(url).get();
+			
 			// importar Elements de la librería jsoup
-			Elements enlaces = documento.select("a");
-			for (Element enlace : enlaces) {
-				System.out.println(enlace.attr("href"));
+			Elements filas = documento.select("TR TD.primera");
+			int i = 0;
+			for (Element fila : filas) {
+				i++;
+				Element celda = fila.select("TD.primera > a").first();
+				Elements celdavalor = fila.select("TD.primera ~ TD");
+				System.out.print("Valor: "+ i +". " + celda.text());
+				//System.out.println(": "+ celdavalor.first().text());
+				for (Element cel: celdavalor) {
+					System.out.println(cel.data());
+				}
 			}
 			
 		} catch (IOException e) {
