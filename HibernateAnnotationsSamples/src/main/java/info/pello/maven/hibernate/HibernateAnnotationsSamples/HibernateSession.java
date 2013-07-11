6ab4cec83cf0f347c3cfb63d0cc083782f5223a7
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@26be5ec8-3439-2871-35d6-f44324df5b0d>
2013-07-11 21:45:27 +0000

    Nice cheesecake, I guess

---
+++ HibernateAnnotationsSamples/src/main/java/info/pello/maven/hibernate/HibernateAnnotationsSamples/HibernateSession.java
@@ -0,0 +1,44 @@
+
+package info.pello.maven.hibernate.HibernateAnnotationsSamples;
+
+/**
+ * @author root
+ *
+ */
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+  
+/**
+ * provide Hibernate Session factory
+ * @author Pello Xabier Altadill Izura
+ * @greetz family
+ */
+public class HibernateSession {
+  
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+  
+    /**
+     * Based on hibernate.cfg.xml configuration creates
+     * a SessionFactory
+     * @return the session factory
+     */
+    private static SessionFactory buildSessionFactory() {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();        
+            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            return sessionFactory;
+    }
+  
+    /**
+     * this gives the desired session factory
+     * @return hibernate Session Factory instance
+     */
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
