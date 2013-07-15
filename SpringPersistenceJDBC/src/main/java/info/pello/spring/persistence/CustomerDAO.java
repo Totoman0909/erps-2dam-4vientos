e5255958c0c66233b6edb03cd94e67e85af6156b
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@26be5ec8-3439-2871-35d6-f44324df5b0d>
2013-07-15 21:28:08 +0000

    greetz from the sand to the sands

--- SpringPersistenceJDBC/src/main/java/info/pello/spring/persistence/CustomerDAO.java
+++ SpringPersistenceJDBC/src/main/java/info/pello/spring/persistence/CustomerDAO.java
@@ -1,6 +1,5 @@
 package info.pello.spring.persistence;
 
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
