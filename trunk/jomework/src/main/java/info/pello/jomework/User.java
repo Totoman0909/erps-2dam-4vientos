fb6aeadd607c41e7d7d1cfc608ed66916070cfa3
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@26be5ec8-3439-2871-35d6-f44324df5b0d>
2013-07-22 23:42:36 +0000

    Now with validation, next the i18n like jquery

--- trunk/jomework/src/main/java/info/pello/jomework/User.java
+++ trunk/jomework/src/main/java/info/pello/jomework/User.java
@@ -3,6 +3,8 @@
  */
 package info.pello.jomework;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 /**
  * Represents User entity
@@ -11,8 +13,15 @@ package info.pello.jomework;
  */
 public class User {
 	private Long id;
+	
+	@Size(min=4,max=30,message="Login must be between 4 and 40 characters long")
+	@Pattern(regexp="[A-Za-z0-9]+",message="Must contain conly chars and numbers")
 	private String login;
+
+	@Size(min=8,max=15,message="Passowrd must be between 8 and 15 characters long")
 	private String password;
+
+	@Size(min=10,max=255,message="Description must be between 10 and 255 characters long")
 	private String description;
 	
 	/**
@@ -31,7 +40,7 @@ public class User {
 	 */
 	public User(Long id, String login, String password, String description) {
 		this.id = id;
-		this.login = login;
+		this.login = login;		
 		this.password = password;
 		this.description = description;
 	}
