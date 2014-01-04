ad8d171be492ecbd8b4a75a358b51f8f7cf10e56
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@26be5ec8-3439-2871-35d6-f44324df5b0d>
2014-01-04 00:19:57 +0000

    greetz for you

---
+++ JavaKartSpringAspect/src/main/java/info/pello/javakart/Main.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package info.pello.javakart;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Pello Altadill
+ * @greetz for the blue mug
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("javakart.xml");
+        
+        Race race = (Race) context.getBean("race");
+        
+        System.out.println(race.toString());
+        race.run();
+        System.out.println(race.showResult());
+
+	}
+
+}
