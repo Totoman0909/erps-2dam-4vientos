96c21fbed8ccb6eab334341c9b321288f9c63268
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@26be5ec8-3439-2871-35d6-f44324df5b0d>
2014-01-26 17:32:07 +0000

    ok, greetz for you

--- HelloSpringMvc/src/main/java/info/pello/mvc/HelloController.java
+++ HelloSpringMvc/src/main/java/info/pello/mvc/HelloController.java
@@ -7,7 +7,6 @@ package info.pello.mvc;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 /**
